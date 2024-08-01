6a62890190e1e18feb6308aaa8da15400a13d42a
Christian Alvarado <christian-alv@hotmail.com>
2024-07-31 20:47:34 -0600

    update: Instalacion y actualizacion de paquetes. + archivo .env

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    __VALUE__: `"${process.env.VALUE}"` // wrapping in "" since it's a string
+  },
   plugins: [
     vue(),
   ],
